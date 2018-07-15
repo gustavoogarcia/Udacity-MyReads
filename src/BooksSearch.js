8d44c80eaec8b735573e47a6844aa55b3b1fc052
Gustavo Garcia <gustavo.ogarcia@gmail.com>
2018-07-15 13:40:33 -0300

    test fix

--- src/BooksSearch.js
+++ src/BooksSearch.js
@@ -9,33 +9,32 @@ class BooksSearch extends React.Component {
     render() {
         let { query, updateQuery, clearQuery, changeShelf, searchedBooks } = this.props
         
-        if (searchedBooks) {
-            return (
-                <div className="search-books">
-                    <div className="search-books-bar">
-                        <Link to="" className="close-search">Close</Link>
-                        <div className="search-books-input-wrapper">
-                            <input type="text" placeholder="Search by title or author" value={query} onChange={(e) => updateQuery(e.target.value)}/>
-                        </div>
-                    </div>
-                    <If test={searchedBooks}>
-                        <div className="search-info">
-                            <div>Now showing {searchedBooks.length} Books on your search!</div>
-                            <button onClick={clearQuery}>Clean search</button>
-                        </div>
-                        <div className="search-books-results">
-                            <ol className="books-grid">
-                                {searchedBooks.map((book) => (
-                                        <BookItem key={book.title} changeShelf={changeShelf} book={book} />
-                                    )
-                                )}
-                            </ol>
-                        </div>
-                    </If>
-                </div>
         
-            )
-        }
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link to="" className="close-search">Close</Link>
+                    <div className="search-books-input-wrapper">
+                        <input type="text" placeholder="Search by title or author" value={query} onChange={(e) => updateQuery(e.target.value)}/>
+                    </div>
+                </div>
+                <If test={searchedBooks}>
+                    <div className="search-info">
+                        <div>Now showing {searchedBooks.length} Books on your search!</div>
+                        <button onClick={clearQuery}>Clean search</button>
+                    </div>
+                    <div className="search-books-results">
+                        <ol className="books-grid">
+                            {searchedBooks.map((book) => (
+                                    <BookItem key={book.title} changeShelf={changeShelf} book={book} />
+                                )
+                            )}
+                        </ol>
+                    </div>
+                </If>
+            </div>
+    
+        )
     }
 };
 
--- src/BooksSearch.test.js
+++ src/BooksSearch.test.js
@@ -3,7 +3,7 @@ import { shallow, mount } from 'enzyme';
 import BooksSearch from './BooksSearch';
 
 const props = {
-    books: [
+    searchedBooks: [
         { title: "Titulo Teste", authors: "Autor Teste", imageLinks: { smallThumbnail: "url test" }, shelf: 'read'  },
         { title: "Titulo2 Teste", authors: "Autor2 Teste", imageLinks: { smallThumbnail: "url2 test" }, shelf: 'read'  },
         { title: "Titulo3 Teste", authors: "Autor3 Teste", imageLinks: { smallThumbnail: "url3 test" }, shelf: 'toRead'  },
@@ -22,18 +22,11 @@ describe('[Component] BooksSearch', () => {
         expect(shallow(<BooksSearch {...props} />));
     });
 
-    it('Maps through the queryed books', () => {
+    it('Maps through the books', () => {
         const wrapper = mount(
             <BooksSearch {...props}/>
         );
         expect(wrapper.find('li')).toHaveLength(3);
-    })
-
-    it('Maps through the books', () => {
-        const wrapper = mount(
-            <BooksSearch query="3" {...props}/>
-        );
-        expect(wrapper.find('li')).toHaveLength(1);
     })
 
     it('Calls updateQuery on input change', () => {
